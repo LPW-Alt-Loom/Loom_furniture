bc4dada546d58f6bb5126ef2cc302580371c2e6a
Raph_Lem <raphael.lemoine@edu.univ-fcomte.fr>
2022-12-08 14:20:30 +0100

    Implement vueJs

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
+
+import "./scss/main.scss";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
